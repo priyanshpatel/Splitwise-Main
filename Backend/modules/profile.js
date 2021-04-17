74b43b22f3edbc5115dd38e6af7a8a800f2389a3
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-04-16 23:03:19 -0700

    update profile api

--- Backend/modules/profile.js
+++ Backend/modules/profile.js
@@ -56,4 +56,46 @@ router.get('/:userId', async (req, res) => {
     }
 });
 
+router.post('/update', uploadGroupImage.single("profilePicture"), async (req, res) => {
+    const userId = req.body.userId;
+    const userName = req.body.userName;
+    const userEmail = req.body.userEmail;
+    const phoneNumber = req.body.phoneNumber;
+    const currency = req.body.currency;
+    const timezone = req.body.timezone;
+    const language = req.body.language;
+
+    let imagePath = null;
+    try{
+    if (req.file) {
+        imagePath = req.file.path.substring(req.file.path.indexOf("/") + 1);
+    }
+    console.log("Inside update profile post");
+    
+    let userSchemaUpd = await userSchema.updateOne(
+        { _id: userId},
+        {
+            $set: {
+                userName: userName,
+                userEmail: userEmail,
+                phoneNumber: phoneNumber,
+                currency: currency,
+                timezone: timezone,
+                language: language
+            }
+        }
+    )
+
+    console.log("Profile Updated", userSchemaUpd)
+    res.status(200).send(userSchemaUpd)
+    } catch(error){
+        console.log("error while updating profile", error)
+        if (error.errno == 1062) {
+            res.status(201).send('Email ID already exists');
+        } else {
+            res.status(500).send(error);
+        }
+    }
+});
+
 module.exports = router;

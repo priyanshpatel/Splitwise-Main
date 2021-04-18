f03a0faac04d51a99594cb7c8bbc2b2414be4d24
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-04-18 15:29:33 -0700

    navbar changes

--- frontend/src/components/LandingPage/Navbar.js
+++ frontend/src/components/LandingPage/Navbar.js
@@ -26,60 +26,69 @@ class Navbar extends Component {
         if (cookie.load('userId')) {
             return (
                 <BrowserRouter>
-                <div>
-                    <nav class="navbar navbar-expand-lg navbar-light" style={{
-                        background: "#D32323",
-                        background: "#59cfa7",
-                        width: "100%",
-                        height: "50px",
-                    }}>
-                        <div class="container">
-                            <a class="navbar-brand" href="/">
-                                {/* <img src="https://lh3.googleusercontent.com/proxy/u5Jk4tgNiM-h3mYXjxrqeDClx4Qp5tyiMvrAdPrqaoaBt_obioNdG1FX1wL6K8yKsUyYpPEbp-zfcxeynL4K-RQiQ5hW-QBJWMURbAsn_NkqaFuOH0I" width="40" height="40" class="d-inline-block align-top" alt=""/> */}
-                                <img src={splitwise_logo} width="40" height="40" class="d-inline-block align-top" alt="" href="/" />
-                                <span style={{ paddingLeft: "10px", color: "white" }}><strong>Splitwise</strong></span>
-                            </a>
-                            <ul class="nav navbar-nav navbar-right">
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "white" }}>
-                                        <em>{cookie.load('userName')}</em>
-                                    </a>
-                                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <li><a className="dropdown-item" href="/">Dashboard</a></li>
-                                        <li><a className="dropdown-item" href="/recentactivity">Recent activity</a></li>
-                                        <li><hr class="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="/mygroups">My groups</a></li>
-                                        <li><a className="dropdown-item" href="/creategroup">Create a group</a></li>
-                                        <li><hr class="dropdown-divider" /></li>
-                                        <li><a className="dropdown-item" href="/profile">Your account</a></li>
-                                    </ul>
-                                </li>
+                    <div>
+                        <nav class="navbar navbar-expand-lg navbar-light" style={{
+                            background: "#D32323",
+                            background: "#59cfa7",
+                            width: "100%",
+                            height: "50px",
+                        }}>
+                            <div class="container">
+                                <a class="navbar-brand" href="/">
+                                    {/* <img src="https://lh3.googleusercontent.com/proxy/u5Jk4tgNiM-h3mYXjxrqeDClx4Qp5tyiMvrAdPrqaoaBt_obioNdG1FX1wL6K8yKsUyYpPEbp-zfcxeynL4K-RQiQ5hW-QBJWMURbAsn_NkqaFuOH0I" width="40" height="40" class="d-inline-block align-top" alt=""/> */}
+                                    <img src={splitwise_logo} width="40" height="40" class="d-inline-block align-top" alt="" href="/" />
+                                    <span style={{ paddingLeft: "10px", color: "white" }}><strong>Splitwise</strong></span>
+                                </a>
 
-                                <a class="btn btn-primary" type="button" href="/" onClick={this.handleLogout} style={{ backgroundColor: "#59cfa7", border: "none" }}>Logout</a>
-                            </ul>
-                        </div>
-                    </nav>
-                </div>
+                                {/* <div class="collapse navbar-collapse" id="navbarNav">
+                                    <ul class="navbar-nav ml-auto">
+                                        <li class="nav-item">
+                                            <Link className="dropdown-item" to="/dashboard">Dashboard</Link>
+                                        </li>
+                                    </ul>
+                                </div> */}
+
+                                <ul class="nav navbar-nav navbar-right">
+                                    <li class="nav-item dropdown">
+                                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false" style={{ color: "white" }}>
+                                            <em>{cookie.load('userName')}</em>
+                                        </a>
+                                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                            <li><Link className="dropdown-item" to="/">Dashboard</Link></li>
+                                            <li><Link className="dropdown-item" to="/recentactivity">Recent activity</Link></li>
+                                            <li><hr class="dropdown-divider" /></li>
+                                            <li><Link className="dropdown-item" to="/mygroups">My groups</Link></li>
+                                            <li><Link className="dropdown-item" to="/creategroup">Create a group</Link></li>
+                                            <li><hr class="dropdown-divider" /></li>
+                                            <li><Link className="dropdown-item" to="/profile">Your account</Link></li>
+                                        </ul>
+                                    </li>
+
+                                    <a class="btn btn-primary" type="button" href="/" onClick={this.handleLogout} style={{ backgroundColor: "#59cfa7", border: "none" }}>Logout</a>
+                                </ul>
+                            </div>
+                        </nav>
+                    </div>
                 </BrowserRouter>
             )
         } else {
             return (
                 <BrowserRouter>
-                <div>
-                    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                        <div class="container">
-                            <a class="navbar-brand" href="#">
-                                {/* <img src="https://lh3.googleusercontent.com/proxy/u5Jk4tgNiM-h3mYXjxrqeDClx4Qp5tyiMvrAdPrqaoaBt_obioNdG1FX1wL6K8yKsUyYpPEbp-zfcxeynL4K-RQiQ5hW-QBJWMURbAsn_NkqaFuOH0I" width="40" height="40" class="d-inline-block align-top" alt=""/> */}
-                                <img src={splitwise_logo} width="40" height="40" class="d-inline-block align-top" alt="" href="/" />
-                                <span style={{ paddingLeft: "10px" }}><strong>Splitwise</strong></span>
-                            </a>
-                            <nav class="navbar navbar-light bg-light">
-                                <a class="nav-link landing-link" href="/login" style={{ color: "#59cfa7" }}><strong>Login</strong></a>
-                                <a class="btn btn-primary" type="button" href="/signup" style={{ backgroundColor: "#59cfa7", border: "none" }}>Sign up</a>
-                            </nav>
-                        </div>
-                    </nav>
-                </div>
+                    <div>
+                        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                            <div class="container">
+                                <a class="navbar-brand" href="#">
+                                    {/* <img src="https://lh3.googleusercontent.com/proxy/u5Jk4tgNiM-h3mYXjxrqeDClx4Qp5tyiMvrAdPrqaoaBt_obioNdG1FX1wL6K8yKsUyYpPEbp-zfcxeynL4K-RQiQ5hW-QBJWMURbAsn_NkqaFuOH0I" width="40" height="40" class="d-inline-block align-top" alt=""/> */}
+                                    <img src={splitwise_logo} width="40" height="40" class="d-inline-block align-top" alt="" href="/" />
+                                    <span style={{ paddingLeft: "10px" }}><strong>Splitwise</strong></span>
+                                </a>
+                                <nav class="navbar navbar-light bg-light">
+                                    <a class="nav-link landing-link" href="/login" style={{ color: "#59cfa7" }}><strong>Login</strong></a>
+                                    <a class="btn btn-primary" type="button" href="/signup" style={{ backgroundColor: "#59cfa7", border: "none" }}>Sign up</a>
+                                </nav>
+                            </div>
+                        </nav>
+                    </div>
                 </BrowserRouter>
             )
         }

37646a5da88f90137a95a0beceec4ae97228a246
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-04-17 17:48:04 -0700

    dashboard you owe

--- Backend/modules/dashboard.js
+++ Backend/modules/dashboard.js
@@ -77,7 +77,51 @@ router.get('/you_are_owed/:userId', async (req, res) => {
     const userId = req.params.userId
     let resObj = {}
     let resArray = []
-    console.log(userId)
+    try {
+        let tranSchemaDoc1 = await transactionSchema.aggregate([
+            {
+                $match: {
+                    $and: [
+                        { paidByUserId: mongoose.Types.ObjectId(userId) },
+                        { settleFlag: 'N' },
+                        { tranType: "6" }
+                    ]
+                }
+            },
+            {
+                $group: {
+                    _id: "$paidForUserId",
+                    total: { $sum: "$amount" }
+                }
+            }])
+        for (const doc of tranSchemaDoc1) {
+            let userSchemaDoc = await userSchema.findOne(
+                { _id: doc._id },
+                {
+                    userName: 1,
+                    currency: 1,
+                    userEmail: 1
+                }
+            )
+            resObj._id = userSchemaDoc._id
+            resObj.userEmail = userSchemaDoc.userEmail
+            resObj.userName = userSchemaDoc.userName
+            resObj.currency = userSchemaDoc.currency
+            resObj.totalAmount = doc.total
+
+            resArray.push(resObj)
+        }
+        res.status(200).send(resArray)
+    } catch (error) {
+        console.log("Error while getting you are owed", error)
+        res.status(500).send(error)
+    }
+});
+
+router.get('/you_owe/:userId', async (req, res) => {
+    const userId = req.params.userId
+    let resObj = {}
+    let resArray = []
     try {
         let tranSchemaDoc1 = await transactionSchema.aggregate([
             {
@@ -118,14 +162,5 @@ router.get('/you_are_owed/:userId', async (req, res) => {
         res.status(500).send(error)
     }
 });
-
-// {
-//     $lookup: {
-//         from: "users",
-//         localField: "_id",
-//         foreignField: "_id",
-//         as: "usersArray"
-//     }
-// },
 
 module.exports = router;

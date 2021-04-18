f813f7626a5e0cd5e4895001d894d5014f840705
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-04-17 17:41:48 -0700

    dashboard you are owed

--- Backend/modules/dashboard.js
+++ Backend/modules/dashboard.js
@@ -73,4 +73,59 @@ router.get('/total_dashboard/:userId', async (req, res) => {
     }
 });
 
+router.get('/you_are_owed/:userId', async (req, res) => {
+    const userId = req.params.userId
+    let resObj = {}
+    let resArray = []
+    console.log(userId)
+    try {
+        let tranSchemaDoc1 = await transactionSchema.aggregate([
+            {
+                $match: {
+                    $and: [
+                        { paidForUserId: mongoose.Types.ObjectId(userId) },
+                        { settleFlag: 'N' },
+                        { tranType: "6" }
+                    ]
+                }
+            },
+            {
+                $group: {
+                    _id: "$paidByUserId",
+                    total: { $sum: "$amount" }
+                }
+            }])
+        for (const doc of tranSchemaDoc1) {
+            let userSchemaDoc = await userSchema.findOne(
+                { _id: doc._id },
+                {
+                    userName: 1,
+                    currency: 1,
+                    userEmail: 1
+                }
+            )
+            resObj._id = userSchemaDoc._id
+            resObj.userEmail = userSchemaDoc.userEmail
+            resObj.userName = userSchemaDoc.userName
+            resObj.currency = userSchemaDoc.currency
+            resObj.totalAmount = doc.total
+
+            resArray.push(resObj)
+        }
+        res.status(200).send(resArray)
+    } catch (error) {
+        console.log("Error while getting you are owed", error)
+        res.status(500).send(error)
+    }
+});
+
+// {
+//     $lookup: {
+//         from: "users",
+//         localField: "_id",
+//         foreignField: "_id",
+//         as: "usersArray"
+//     }
+// },
+
 module.exports = router;

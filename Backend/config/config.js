db3cb7e46707667bee13ba42e64f43f39e41072e
Priyansh Patel <43277093+priyanshpatel@users.noreply.github.com>
2021-04-24 15:53:49 -0700

    deployment url

--- Backend/config/config.js
+++ Backend/config/config.js
@@ -1,8 +1,8 @@
-let frontend_url = "https://54.151.116.207:3000"
+let frontend_url = "http://54.151.116.207:3000"
 // let mongodb_string_test = "mongodb+srv://admin:saiyangoku@cluster0.p6nod.mongodb.net/test?authSource=admin&replicaSet=atlas-cbegpg-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true"
 let mongodb_string = "mongodb+srv://admin:saiyangoku@splitwisecluster.idknk.mongodb.net/splitwise_mongo?retryWrites=true&w=majority"
 let secret = "CMPE_273_Lab_2"
-let kafka_url = "https://54.151.116.207:2181"
+let kafka_url = "http://54.151.116.207:2181"
 
 module.exports = {
     frontend_url: frontend_url,
